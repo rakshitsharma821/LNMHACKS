02c3d55e9fb131f297d280a7776c7a1b251f692f
Rakshit Sharma <rakshitsharma668@gmail.com>
2026-01-25 08:59:25 +0530

    Delete backend directory

--- backend/routers/resultRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getUserResult } = require('../controllers/resultcontroller.js');
-const protect = require('../middleware/auth.js');
-const router = express.Router();
-
-router.get('/:userId', protect, getUserResult);
-
-module.exports = router;
